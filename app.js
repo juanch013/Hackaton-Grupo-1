8c2d7f91bbfb97c04ba7037e4cf7c328024fbff1
gabofarex <gabofarex@gmail.com>
2022-11-03 12:53:31 -0300

    agregando rutas de product y recipue

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const PORT = process.env.PORT;
 
 app.use(express.json());
 
+app.use("/api/products", productRoutes);
+app.use("/api/recipes", recipeRoutes);
+
 const server = app.listen(PORT, () => {
     console.log(`server corriendo en ${PORT}`);
     //sequelize.sync({ alter: true });
